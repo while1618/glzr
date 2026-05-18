e886b0293d7328af012dfd8e9d651b4c6640dc82
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2026-05-18 02:21:03 +0200

    fix sr layout

--- zebar/bar/bar/src/InputMethod/InputMethodStatus.tsx
+++ zebar/bar/bar/src/InputMethod/InputMethodStatus.tsx
@@ -22,8 +22,8 @@ const InputMethodStatus: Component<InputMethodStatusProps> = (props) => {
     if (lower.startsWith("fr")) return "fr";
     if (lower.startsWith("de")) return "de";
     if (lower.startsWith("es")) return "es";
-    if (lower.startsWith("sr")) return "sr";
     if (lower.startsWith("sr_cyrl")) return "sr_cyrl";
+    if (lower.startsWith("sr")) return "sr";
     return lower.slice(0, 2).toUpperCase();
   };
 
